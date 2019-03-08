d6d7fdf7845b65842e929dd52f1259ca1ab0e1b7
Jose Riguera <jriguera@gmail.com>
2019-03-08 16:10:50 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cf-grafana-buildpack
+
 PoC
+
+# Configuration
+
+http://docs.grafana.org/installation/configuration/
+
+* Using environment variables in the manifest.
+* Creating a `grafana.ini` file in the root folder of the app. 
+* Root folder of the app is `provisioning` folder, so you can create these directories: `datasources`, `dashboards`, `notifiers`, `plugins`.
+
+
+# Database support
+
+TODO: Via service brokers
+
+
+# Author
+
+Jose Riguera
---
+++ grafana.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+export GRAFANA_ROOT=${HOME}/deps/grafana
+export APP_ROOT=${HOME}/app
+export DOMAIN=$(echo ${VCAP_APPLICATION} | jq ".[\"uris\"][0]" --raw-output)
+export PATH=$PATH:${GRAFANA_ROOT}/bin
+
+
+# exec grafana and create the datasources
+launch() {
+    echo "Launching: '$@'"
+    # Exec process
+    echo "* -- START -- PID=$$"
+    (
+        echo "* Process environment was:"
+        echo "* Command line of pid $$ was:"
+        echo "$@"
+        echo "* -- $(date) --"
+        {
+            exec $@  2>&1;
+        }
+    ) &
+    pid=$!
+    sleep 30
+    if ! ps -p $pid >/dev/null 2>&1; then
+        echo
+        echo "Error launching '$@'."
+        rvalue=1
+    else
+        wait $pid 2>/dev/null
+        rvalue=$?
+    fi
+    echo "* -- END -- RC=$rvalue"
+    return $rvalue
+}
+
+
+cd ${GRAFANA_ROOT}
+
+echo "Launching grafana server..."
+if [ -f "${APP_ROOT}/grafana.ini" ]
+then
+    launch ./bin/grafana-server -config=${APP_ROOT}/grafana.ini
+else
+    launch ./bin/grafana-server
+fi
+
