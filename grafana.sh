395e9140c141128031eda8df8d563f3676c0fc3f
Jose Riguera <jriguera@gmail.com>
2019-03-14 17:50:47 +0100

    Test sql_proxy

--- grafana.sh
+++ grafana.sh
@@ -3,8 +3,9 @@
 export DOMAIN=$(echo ${VCAP_APPLICATION} | jq ".[\"uris\"][0]" --raw-output)
 
 export GRAFANA_ROOT=/home/vcap/deps/grafana
+export SQLPROXY_ROOT=/home/vcap/deps/cloud_sql_proxy
 export APP_ROOT=${HOME}
-export PATH=$PATH:${GRAFANA_ROOT}/bin
+export PATH=${PATH}:${GRAFANA_ROOT}/bin:${SQLPROXY_ROOT}
 
 ###
 
@@ -14,6 +15,8 @@ randomstring() {
 
 # exec grafana and create the datasources
 launch() {
+    local backgroud=${1}
+    shift
     echo "Launching: '$@'"
     # Exec process
     echo "* -- START -- PID=$$"
@@ -33,8 +36,13 @@ launch() {
         echo "Error launching '$@'."
         rvalue=1
     else
-        wait $pid 2>/dev/null
-        rvalue=$?
+        if [[ -z "${background}" ]]
+        then
+            wait $pid 2>/dev/null
+            rvalue=$?
+        else
+            rvalue=0
+        fi
     fi
     echo "* -- END -- RC=$rvalue"
     return $rvalue
@@ -42,7 +50,7 @@ launch() {
 
 
 echo "Setting environment variables ..."
-if [ -z "${SECRET_KEY}" ]
+if [[ -z "${SECRET_KEY}" ]]
 then
     export SECRET_KEY=$(randomstring)
     echo "######################################################################"
@@ -56,6 +64,15 @@ fi
 export ADMIN_USER=${ADMIN_USER:-admin}
 export ADMIN_PASS=${ADMIN_PASS:-admin}
 export EMAIL=${EMAIL:-grafana@$DOMAIN}
+
+
+echo "Launching local sql proxy ..."
+if [ -f "${APP_ROOT}/auth.json" ]
+then
+    launch background cloud_sql_proxy -instances=sn-paas-sb-gcp:europe-west4:pcf-sb-2-1552482443106729127=tcp:3306 -credential_f
+ile "${APP_ROOT}/auth.json" -log_debug_stdout -verbose
+fi
+
 
 echo "Launching grafana server..."
 cd ${GRAFANA_ROOT}
