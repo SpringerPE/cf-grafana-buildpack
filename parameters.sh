bc05561a81aaa79a410c28d07623e1d21b1e5e23
Jose Riguera <jriguera@gmail.com>
2019-08-09 11:30:04 +0200

    Update grafana to 6.3.2

--- parameters.sh
+++ parameters.sh
@@ -1,7 +1,7 @@
 # Variables
 
 # default versions
-GRAFANA_VERSION="${GRAFANA_VERSION:-6.2.2}"
+GRAFANA_VERSION="${GRAFANA_VERSION:-6.3.2}"
 CLOUDSQL_PROXY_VERSION="${CLOUDSQL_PROXY_VERSION:-1.13}"
 
 # dependencies paths
