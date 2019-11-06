ad961d739e851141815dd394a2951a571efdb43f
Jose Riguera <jriguera@gmail.com>
2019-11-06 16:04:00 +0100

    bump default grafana version to 6.4.4

--- parameters.sh
+++ parameters.sh
@@ -1,7 +1,7 @@
 # Variables
 
 # default versions
-GRAFANA_VERSION="${GRAFANA_VERSION:-6.4.2}"
+GRAFANA_VERSION="${GRAFANA_VERSION:-6.4.4}"
 CLOUDSQL_PROXY_VERSION="${CLOUDSQL_PROXY_VERSION:-1.13}"
 
 # dependencies paths
