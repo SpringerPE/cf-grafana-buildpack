cf17a7b98c460263fd930fe79d045eb2f28842fd
Jose Riguera <jriguera@gmail.com>
2019-11-27 11:04:35 +0100

    improve scripts to make them multi-buildpack aware

--- parameters.sh
+++ parameters.sh
@@ -1,8 +1,12 @@
-# Variables
+# Variables, this file is designed to be sourced by supply
 
 # default versions
 GRAFANA_VERSION="${GRAFANA_VERSION:-6.4.4}"
 CLOUDSQL_PROXY_VERSION="${CLOUDSQL_PROXY_VERSION:-1.13}"
+
+# Download URLS
+GRAFANA_DOWNLOAD_URL="https://dl.grafana.com/oss/release/grafana-${GRAFANA_VERSION}.linux-amd64.tar.gz"
+CLOUDSQL_PROXY_DOWNLOAD_URL="https://storage.googleapis.com/cloudsql-proxy/v${CLOUDSQL_PROXY_VERSION}/cloud_sql_proxy.linux.amd64"
 
 # dependencies paths
 GRAFANA_DIR="${DEPS_DIR}/${DEPS_IDX}/grafana"
