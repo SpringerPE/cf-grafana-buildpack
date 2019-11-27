b2c07469fa24b52a57727bdc3c7dab300cc7e25a
Jose Riguera <jriguera@gmail.com>
2019-11-27 18:20:12 +0100

    cosmetic

--- grafana.sh
+++ grafana.sh
@@ -333,12 +333,14 @@ set_homedashboard() {
     if [[ "${dashboard_httpcode[1]}" == "200" ]]
     then
         dashboard_id=$(jq '.dashboard.id' <<<"${dashboard_httpcode[0]}")
-        echo -n "Defining default home dashboard id ${dashboard_id} for org ${HOME_ORG_ID}: "
-        curl -s -X PUT -u "${ADMIN_USER}:${ADMIN_PASS}" \
+        output=$(
+            curl -s -X PUT -u "${ADMIN_USER}:${ADMIN_PASS}" \
                  -H 'Content-Type: application/json;charset=UTF-8' \
                  -H "X-Grafana-Org-Id: ${HOME_ORG_ID}" \
                  --data-binary "{\"homeDashboardId\": ${dashboard_id}}" \
                  "http://127.0.0.1:${PORT}/api/org/preferences"
+        )
+        echo "Defined default home dashboard id ${dashboard_id} for org ${HOME_ORG_ID}: ${output}"
     elif [[ "${dashboard_httpcode[1]}" == "404" ]]
     then
         echo "No default home dashboard for org ${HOME_ORG_ID} has been found"
