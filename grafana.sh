8f75cf25eddad4984a82b199b5d88316b1d3d061
Hiral Visaria <67199209+hiral-visaria@users.noreply.github.com>
2023-10-19 14:01:32 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -166,7 +166,12 @@ set_env_DB() {
         DB_TLS="false"
     elif [[ "${DB_TYPE}" == "postgres" ]]
     then
-        DB_PORT="5432"
+	if ! DB_PORT=$(jq -r -e '.credentials.Port' <<<"${db}")
+    	then
+		DB_NAME=$(jq -r -e '.credentials.uri |
+	    		split("://")[1] | split("/")[1] |
+	    		split("?")[0]' <<<"${db}") || DB_PORT=''
+    	fi
         uri="${uri}:${DB_PORT}"
         DB_TLS="disable"
     fi
